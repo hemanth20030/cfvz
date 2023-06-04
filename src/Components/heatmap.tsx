96eccbd8b22978186b65d8cb927ae48b97d4b3bf
hemanth0107 <hemanth.madala0107@gmail.com>
2023-06-04 22:13:34 +0530

    *

--- src/Components/heatmap.tsx
+++ src/Components/heatmap.tsx
@@ -1,2 +1,13 @@
 //rolled up due to issues in deployment
 //will be able to find in previous commits
+import React from 'react'
+
+function heatmap() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default heatmap
